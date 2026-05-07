f37f2c0e7d099ef097f5284d046f671dd645c34b
Caiolimapa <caiolima.albuquerque@gmail.com>
2026-05-07 18:35:26 -0300

    fix railway flask server

--- Prototipo_Ajudaai/Backend/app.py
+++ Prototipo_Ajudaai/Backend/app.py
@@ -36,4 +36,5 @@ app.register_blueprint(ong_bp, url_prefix='/api')
 
 # Rodar servidor
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
