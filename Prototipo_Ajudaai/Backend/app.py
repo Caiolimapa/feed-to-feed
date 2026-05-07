485e1cdfe39d45a368a4a962ab5d13dc81a53ce8
Caiolimapa <caiolima.albuquerque@gmail.com>
2026-05-07 18:03:37 -0300

    conectando supabase

--- Prototipo_Ajudaai/Backend/app.py
+++ Prototipo_Ajudaai/Backend/app.py
@@ -1,28 +1,39 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+
 from Models.ong import db
 from routes.ong_routes import ong_bp
+
+# Carregar variáveis ambiente
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-# Definir o caminho correto do banco de dados
-basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(basedir, 'Database', 'db.sqlite3')
+# Configuração PostgreSQL Supabase
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+# SSL obrigatório Supabase
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "connect_args": {
+        "sslmode": "require"
+    }
+}
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Inicializar banco
 db.init_app(app)
 
-# Criar tabelas se não existirem
+# Criar tabelas
 with app.app_context():
     db.create_all()
 
+# Rotas
 app.register_blueprint(ong_bp, url_prefix='/api')
 
+# Rodar servidor
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
-
-#arquivo principal (inicia o servidor)
